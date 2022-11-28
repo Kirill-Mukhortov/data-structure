fbc9b7164115cabfd1b24d4416406fd18686a97b
Kirill Mukhortov <muchortovkirill@gmail.com>
2022-11-29 02:11:51 +0300

    add methods insertBefore and insertAfter

--- list.js
+++ list.js
@@ -31,8 +31,7 @@ class LinkedList {
         return this.#last;
     }
 
-    // Проверка пустой список или нет
-    isEmpty() {
+    get isEmpty() {
         return this.#length === 0 || this.#first === null;
     }
 
@@ -67,7 +66,7 @@ class LinkedList {
 
     // Удаление последнего элемента
     pop() {
-        if (this.isEmpty()) {
+        if (this.isEmpty) {
             throw new Error('List is empty')
         }
 
@@ -82,7 +81,7 @@ class LinkedList {
 
     // Удаление первого элемента
     shift() {
-        if (this.isEmpty()) {
+        if (this.isEmpty) {
             throw new Error('List is empty')
         }
 
@@ -97,6 +96,10 @@ class LinkedList {
 
     // Поиск элемента по значению
     findByValue(value) {
+        if (this.isEmpty) {
+            throw new Error('List is empty')
+        }
+
         let current = this.#first;
 
         while (current.value !== value) {
@@ -131,6 +134,44 @@ class LinkedList {
         }
     }
 
+    // Поиск по значению и вставка перед
+    insertBefore(key, value) {
+        try {
+            const nodeToInsertBefore = this.findByValue(key);
+            const newNode = new Node(value);
+
+            if (nodeToInsertBefore.prev === null) {
+                this.#length += 1;
+                return this.unshift(value);
+            }
+
+            nodeToInsertBefore.prev.setNext(newNode);
+            newNode.setNext(nodeToInsertBefore);
+            this.#length += 1;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Поиск по значению и вставка после
+    insertAfter(key, value) {
+        try {
+            const nodeToInsertAfter = this.findByValue(key);
+            const newNode = new Node(value);
+
+            if (nodeToInsertAfter.next === null) {
+                return this.push(value);
+                this.#length += 1;
+            }
+
+            newNode.setNext(nodeToInsertAfter.next);
+            nodeToInsertAfter.setNext(newNode);
+            this.#length += 1;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     // Очистка связанного списка
     clear() {
         this.#first = null;
@@ -141,13 +182,17 @@ class LinkedList {
 
 const list = new LinkedList();
 list.push(1).push(2).push(3);
-console.log(list.length);
-
-console.log(list.removeByValue(3));
 console.log(list.structure);
-// console.log(list.pop());
+// list.insertBefore(2, 5);
 // console.log(list.structure);
-console.log(list.length);
+list.insertAfter(1, 9);
+console.log(list.structure);
+// console.log(list.findByValue());
+
+// console.log(list.removeByValue(3));
+// console.log(list.pop());
+// console.log(list.pop());
+// console.log(list.length);
 // console.log(list.first.value);
 // console.log(list.last.value);
 // console.log(list.first.next.value);
