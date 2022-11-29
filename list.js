bb6043d488ad200639e0f1a72ece92d64e1455ca
Kirill Mukhortov <muchortovkirill@gmail.com>
2022-11-29 03:27:08 +0300

    refactor insertBefore and insertAfter and add methode reverse

--- list.js
+++ list.js
@@ -37,7 +37,7 @@ class LinkedList {
 
     // Вставка в конец
     push(value) {
-        const newNode = new Node(value);
+        let newNode = new Node(value);
 
         if (!this.#first && !this.#last) {
             this.#first = newNode;
@@ -50,17 +50,21 @@ class LinkedList {
         this.#last = newNode;
 
         this.#length += 1;
+        newNode = null;
+
         return this;
     }
 
     // Вставка в начало
     unshift(value) {
-        const newNode = new Node(value);
-        const current = this.#first;
+        let newNode = new Node(value);
+        let current = this.#first;
 
         this.#first = newNode;
         this.#first.setNext(current);
         this.#length += 1;
+        newNode = current = null;
+
         return this;
     }
 
@@ -128,7 +132,9 @@ class LinkedList {
 
             nodeToRemove.next.setPrev(nodeToRemove.prev);
             nodeToRemove.prev.setNext(nodeToRemove.next);
+            nodeToRemove = null;
             this.#length -= 1;
+
         } catch (error) {
             console.log(error);
         }
@@ -137,8 +143,8 @@ class LinkedList {
     // Поиск по значению и вставка перед
     insertBefore(key, value) {
         try {
-            const nodeToInsertBefore = this.findByValue(key);
-            const newNode = new Node(value);
+            let nodeToInsertBefore = this.findByValue(key);
+            let newNode = new Node(value);
 
             if (nodeToInsertBefore.prev === null) {
                 this.#length += 1;
@@ -146,8 +152,12 @@ class LinkedList {
             }
 
             nodeToInsertBefore.prev.setNext(newNode);
+            newNode.setPrev(nodeToInsertBefore.prev);
             newNode.setNext(nodeToInsertBefore);
+            newNode.next.setPrev(newNode);
+            nodeToInsertBefore = newNode = null;
             this.#length += 1;
+
         } catch (error) {
             console.log(error);
         }
@@ -156,20 +166,57 @@ class LinkedList {
     // Поиск по значению и вставка после
     insertAfter(key, value) {
         try {
-            const nodeToInsertAfter = this.findByValue(key);
-            const newNode = new Node(value);
+            let nodeToInsertAfter = this.findByValue(key);
+            let newNode = new Node(value);
 
             if (nodeToInsertAfter.next === null) {
                 return this.push(value);
                 this.#length += 1;
             }
 
+            nodeToInsertAfter.next.setPrev(newNode);
             newNode.setNext(nodeToInsertAfter.next);
-            nodeToInsertAfter.setNext(newNode);
+            newNode.setPrev(nodeToInsertAfter);
+            newNode.prev.setNext(newNode);
+            nodeToInsertAfter = newNode = null;
             this.#length += 1;
+
         } catch (error) {
             console.log(error);
         }
+    }
+
+    // Развернуть список в обратном порядке
+    reverse() {
+        if (this.isEmpty) {
+            throw new Error('List is empty')
+        }
+
+        if (this.#first === this.#last) {
+          return this;
+        }
+
+        let currentNode = this.#first;
+        let prevNode = null;
+        let nextNode = null;
+
+        while (currentNode !== null) {
+            prevNode = currentNode.prev;
+            nextNode = currentNode.next;
+
+            currentNode.setNext(prevNode);
+            currentNode.setPrev(nextNode);
+
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        this.#last = this.#first;
+        this.#first = prevNode;
+
+        currentNode = prevNode = nextNode = null;
+
+        return this;
     }
 
     // Очистка связанного списка
@@ -182,11 +229,18 @@ class LinkedList {
 
 const list = new LinkedList();
 list.push(1).push(2).push(3);
-console.log(list.structure);
+// console.log('0 =>', list.structure);
+list.insertAfter(2, 9);
+console.log('1 =>', list.structure);
+list.reverse();
+console.log('2=>', list.structure);
+// list.reverse();
+// console.log(list.structure);
+
 // list.insertBefore(2, 5);
 // console.log(list.structure);
-list.insertAfter(1, 9);
-console.log(list.structure);
+// list.insertAfter(1, 9);
+// console.log(list.structure);
 // console.log(list.findByValue());
 
 // console.log(list.removeByValue(3));
