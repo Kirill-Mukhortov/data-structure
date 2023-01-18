1271d6a8447e87da7820ff98f4211f32e19b2b05
Kirill Mukhortov <muchortovkirill@gmail.com>
2023-01-19 01:32:14 +0300

    add methods push, shift of queue

--- Queue/queue.js
+++ Queue/queue.js
@@ -1,17 +1,33 @@
-export class Queue {
-    get head() {
+import { LinkedList } from '../LinkedList/list.js';
 
+export class Queue {
+    #queue = new LinkedList();
+
+    get length(){
+        return this.#queue.length;
     }
 
     get isEmpty() {
-
+        return this.#queue.length === 0;
     }
 
-    push() {
+    get head() {
+        if (!this.isEmpty) {
+            return this.#queue.first.value;
+        } else {
+            throw new Error('Queue is empty');
+        }
+    }
 
+    push(value) {
+        this.#queue.insertLast(value);
     }
 
     shift() {
-
+        if (!this.isEmpty) {
+            return this.#queue.removeFirst();
+        } else {
+            throw new Error('Queue is empty');
+        }
     }
 }
