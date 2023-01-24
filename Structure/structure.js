1de99fef3652f35a5f34392bff48ba9a5b850a51
Kirill Mukhortov <muchortovkirill@gmail.com>
2023-01-24 03:33:37 +0300

    create structure 'Structure' based on Map()

---
+++ Structure/structure.js
@@ -0,0 +1,54 @@
+export class Structure {
+    constructor(params) {
+        if (Array.isArray(params) && params.length !== 0 && params !== 'undefined') {
+            this.structure = new Map();
+            for (const element of params) {
+                this.structure.set(element, null);
+            }
+        } else {
+            throw new Error('Params must be an array or it`s length must be greater than 0');
+        }
+    }
+
+    set(key, value) {
+        if (arguments.length < 2) {
+            throw new Error('Need to pass two arguments');
+        }
+
+        if (this.structure.has(key)) {
+            this.structure.set(key, value);
+            return this;
+        } else {
+            throw new Error('No such key in structure');
+        }
+    }
+
+    get(key) {
+        if (this.structure.has(key)) {
+          return this.structure.get(key);
+        } else {
+            throw new Error('No such key in structure');
+        }
+    }
+
+    delete(key) {
+        if (this.structure.has(key)) {
+            this.structure.delete(key);
+            return this;
+        } else {
+            throw new Error('No such key in structure');
+        }
+
+    }
+}
+
+
+const struct = new Structure(['name', 'lastName', 'age']);
+struct.set('name', 'Jack');
+struct.set('lastName', 'Black');
+struct.set('age', 53);
+
+console.log(struct.get('name'));
+console.log(struct.structure);
+console.log(struct.delete('age'));
+console.log(struct.structure);
